9e65191b449744bf0b60cf9f1e1591f11b9052c1
Old Jobobo <jsbrown7@gmail.com>
2022-03-31 17:20:12 -0700

    minor add
    added iron tile and start for creating a random player spawn not on a blocking tile.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -82,4 +82,24 @@ public class PlayerController : MonoBehaviour
         else
             return false;
     }
+
+    Vector3Int FindPlayerSpawn(Tilemap tilemap)
+    {
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            { 
+            
+
+
+            }
+        }
+
+
+
+
+
+                return null;
+    }
 }
